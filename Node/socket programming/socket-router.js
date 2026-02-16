fbfadabdb4ccf73b6e5403676a7c3f1e2175c691
Praneeth Reddy <praneeth.reddy@ittstarglobal.com>
2026-02-16 18:41:11 +0530

    Resolved vanish mode checker issue

--- Node/socket programming/socket-router.js
+++ Node/socket programming/socket-router.js
@@ -8,19 +8,14 @@ module.exports = (io, socket) => {
         sendMessage(io,socket,data)
     })
     socket.on("request_history", ()=>{
-        
         chatHistory(io,socket)
     })
-
-    // PRIVATE CHAT
     socket.on("join_private_room", (data) => {
         joinPrivateRoom(io, socket, data)
     });
-
     socket.on("send_private_message", (data) => {
         sendPrivateMessage(io, socket, data)
     });
-
     socket.on("toggle_vanish_mode", (data) => {
         toggleVanishMode(io, socket, data)
     });
